40dcf05990c4c86eba5fac642aab0f1910706141
BednarStudios <bednarstudios@gmail.com>
2025-06-26 10:30:11 +0000

    updating

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+http.createServer((req, res) => {
+  if (req.url === '/ping') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('pong');
+  } else {
+    res.writeHead(404);
+    res.end('not found');
+  }
+}).listen(process.env.PORT || 3000);
